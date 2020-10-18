8b3e7270ea38c5c8402770406eb53f461125e2f4
Michal <michal.stachura@wp.pl>
2020-10-18 15:26:18 +0200

    add server status checking

--- src/main.cpp
+++ src/main.cpp
@@ -18,13 +18,10 @@ int main(int, char**) {
     std::cout<<server.status()<<std::endl;
 
     std::unordered_map<std::string, std::string> new_employee;
-    new_employee.insert({"imie", "Kuba'; DROP TABLE pracownicy; --"});
-    new_employee.insert({"nazwisko", "Rozpruwacz"});
-    new_employee.insert({"data_urodzenia", "1.08.1795"});
-    new_employee.insert({"stanowisko\"; DROP TABLE pracownicy; --", "rzeźnik pasjonata"});
+    new_employee.insert({"imide", "Kuba"});
+    new_employee.insert({"nazwisko", "a"});
+    new_employee.insert({"data_urodzenia", "1.08.1905"});
+    new_employee.insert({"stanowisko", "brak"});
 
     server.insert_data(new_employee);
-
- 
-
 }
